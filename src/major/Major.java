f4cafa3a5558b3271f05588fecc446b7242d9b68
hiteshsethi <hitesh.28jan@gmail.com>
2014-11-07 19:11:15 +0530

    initial version

---
+++ src/major/Major.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package major;
+
+/**
+ *
+ * @author hitesh
+ */
+public class Major {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
